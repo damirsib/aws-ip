3fa542e1ae75771710c14b84f1943d540f57aa08
damir <sibgatullin@phystech.edu>
2020-07-30 16:22:39 +0300

    Moved frontend to the folder

--- client/script.js
+++ client/script.js
@@ -1,6 +1,8 @@
+const API_INVOKE_URL = 'https://mxqsmxb9j2.execute-api.us-east-1.amazonaws.com/dev'
+const IP_DETECTOR = 'https://api.ipify.org?format=json'
+
 function callAPI(ip) {
     let myHeaders = new Headers();
-
     myHeaders.append("Content-Type", "application/json");
 
     let raw = JSON.stringify({"ip": ip});
@@ -12,13 +14,13 @@ function callAPI(ip) {
         redirect: 'follow'
     };
 
-    fetch("https://mxqsmxb9j2.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+    fetch(API_INVOKE_URL, requestOptions)
         .then(response => response.text())
         .catch(error => console.log('error', error));
 }
 
 function getIP() {
-    fetch('https://api.ipify.org?format=json')
+    fetch(IP_DETECTOR)
         .then(response => response.json())
         .then(data => {	document.getElementById('ip').innerHTML = data.ip;
             callAPI(data.ip); });
